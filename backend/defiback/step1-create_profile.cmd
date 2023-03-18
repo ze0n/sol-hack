e3ef7cf969b4bb8851ffa22d32f7ec5bdf07ff13
konstantin <constantinvk@gmail.com>
2023-03-18 17:09:44 +0100

    Remove param

--- backend/defiback/step1-create_profile.cmd
+++ backend/defiback/step1-create_profile.cmd
@@ -1,4 +1,4 @@
-curl -X POST -H "Content-Type: application/json" -d "{ \"name\": \"Wendy Brain\", \"walletid\": \"987498732\", \"id\": \"3284892\", \"password\": \"kskask\" }" http://localhost:5000/api/signup
+curl -X POST -H "Content-Type: application/json" -d "{ \"name\": \"Wendy Brain\", \"walletid\": \"987498732\", \"password\": \"kskask\" }" http://localhost:5000/api/signup
 
 curl -X GET -H "Content-Type: application/json" -d "" http://localhost:5000/api/users
 
